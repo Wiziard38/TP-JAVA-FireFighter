1c4dce5725c90f406530d6c341e0c01a55eeb946
Mathis Girault <giraulma@ensipc144.ensimag.fr>
2022-10-31 15:42:19 +0100

    robots

--- codeEtudiants/src/io/Robot.java
+++ codeEtudiants/src/io/Robot.java
@@ -1,79 +1,186 @@
 package io;
 
+import io.NatureTerrain;
+
 public abstract class Robot {
 	private Case position;
-	private boolean accesTerrains[] = new boolean[5];
-	private int tailleReservoir;
-	private int vitesseRemplissage;
-	private boolean remplitAdjacentEau;
-	private int interventionUnitaire;
-	private int quantiteEau = 0;
+	private int vitesse;
+	private final double tailleReservoir;
+	private double quantiteEau = 0;
+
 	
-	public Robot(Case position, boolean accesTerrains[], int tailleReservoir, int vitesseRemplissage, boolean remplitAdjacentEau, int interventionUnitaire) {
-		this.position = position;
-		this.accesTerrains = accesTerrains;
+	public Robot(Case init_position, int vitesse, double tailleReservoir) {
+		if (vitesse < 0) {
+			throw new IllegalArgumentException("La vitesse doit etre positive !");
+		}
+
+		this.position = init_position;
+		this.vitesse = this.setVitesse;
 		this.tailleReservoir = tailleReservoir;
-		this.vitesseRemplissage = vitesseRemplissage;
-		this.remplitAdjacentEau = remplitAdjacentEau;
-		this.interventionUnitaire = interventionUnitaire;;
+		
+		this.setEauRestante(tailleReservoir);
 	}
+
 	
 	public Case getPosition() {
 		return this.position;
 	}
 	
 	public void setPosition(Case newPosition) {
+		if (!this.peutDeplacer(newPosition.getNature())) {
+			throw new IllegalArgumentException("Tentative de deplacement sur case interdite !");
 		this.position = newPosition;
 	}
 	
-	public int getEau() {
+	public int getEauRestante() {
 		return this.quantiteEau;
 	}
 	
-	public void setEau(int volume) {
+	public void setEauRestante(double volume) {
+		if (volume < 0) {
+			throw new IllegalArgumentException("Volume d'eau négatif !");
+		}
 		this.quantiteEau = volume;
 	}
-	
-	public abstract double getVitesse(NatureTerrain terrain);
-	
-	public void deverseEau(int volume) {
-		this.quantiteEau -= volume;
-	}
-	
-	public void remplirReservoir() {
-		this.quantiteEau = tailleReservoir;
-	}
-}
 
+	public double deverseEau(double volume) {
+		double EauRestante = this.getEauRestante();
 
-class RobotDrone extends Robot {
-	private int vitesse;
-	private static boolean[] acces = {true, true, true, true, true};
-	
-	public RobotDrone(Case position) {
-		super(position, acces, 10000, 30, false, 30);
-		this.vitesse = 100;
+		if (EauRestante < volume) {
+			this.setEauRestante(0);
+			return EauRestante;
+		}
+		this.setEauRestante(EauRestante-volume);
+		return volume;
 	}
-	
-	public RobotDrone(Case position, int vitesse) {
-		super(position, acces, 10000, 30, false, 30);
+
+	public void setVitesse(int vitesse) {
+		if (vitesse < 0) {
+			throw new IllegalArgumentException("Vitesse négative !");
+		}
 		this.vitesse = vitesse;
 	}
 
-	public double getVitesse(NatureTerrain terrain) {
-		return this.vitesse;
+	public int getVitesse(NatureTerrain nature) {
+		return this.getVitesse();
 	}
+
+
+	public abstract boolean peutDeplacer(NatureTerrain terrain);
+
+	public abstract String getNameRobot();
 }
 
 
 
+public class RobotDrone extends Robot {
+	
+	public RobotDrone(Case position) {
+		super(position, 100, 10000);
+	}
+	
+	public RobotDrone(Case position, int vitesse) {
+		super(position, vitesse, 10000);
+	}
+
+	@Override
+	public void setVitesse(int vitesse) {
+		if (vitesse > 150) {
+			throw new IllegalArgumentException("Vitesse trop grande !");
+		}
+		super.setVitesse(vitesse);
+	}
+
+	@Override
+	public boolean peutDeplacer(NatureTerrain terrain) {
+		return true;
+	}
+
+	@Override
+	public String getNameRobot() {
+		return "Drone";
+	}
+}
 
 
+public class RobotARoues extends Robot {
+	
+	public RobotDrone(Case position) {
+		super(position, 80, 5000);
+	}
+	
+	public RobotDrone(Case position, int vitesse) {
+		super(position, vitesse, 5000);
+	}
 
 
+	@Override
+	public boolean peutDeplacer(NatureTerrain terrain) {
+		return (terrain == NatureTerrain.TERRAIN_LIBRE || terrain == NatureTerrain.HABITAT);
+	}
+
+	@Override
+	public String getNameRobot() {
+		return "Robot a roues";
+	}
+}
 
 
+public class RobotAChenilles extends Robot {
+	
+	public RobotDrone(Case position) {
+		super(position, 60, 2000);
+	}
+	
+	public RobotDrone(Case position, int vitesse) {
+		super(position, vitesse, 2000);
+	}
 
 
+	@Override
+	public void setVitesse(int vitesse) {
+		if (vitesse > 80) {
+			throw new IllegalArgumentException("Vitesse trop grande !");
+		}
+		super.setVitesse(vitesse);
+	}
+
+	@Override
+	public int getVitesse(NatureTerrain nature) {
+		return (nature == NatureTerrain.FORET ? this.getVitesse() / 2 : this.getVitesse());
+	}
+
+	@Override
+	public boolean peutDeplacer(NatureTerrain terrain) {
+		return !(terrain == NatureTerrain.EAU || terrain == NatureTerrain.ROCHE);
+	}
+
+	@Override
+	public String getNameRobot() {
+		return "Robot a chenilles";
+	}
+}
 
 
+public class RobotAPattes extends Robot {
+	
+	public RobotDrone(Case position) {
+		super(position, 30, 2000);
+	}
+
+
+	@Override
+	public int getVitesse(NatureTerrain nature) {
+		return (nature == NatureTerrain.ROCHE ? 30 : 10);
+	}
+
+	@Override
+	public boolean peutDeplacer(NatureTerrain terrain) {
+		return !(terrain == NatureTerrain.EAU);
+	}
+
+	@Override
+	public String getNameRobot() {
+		return "Robot a pattes";
+	}
+}
